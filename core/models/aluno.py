1578b16cd6f75858119405b74b5211a98a2fa3cf
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2026-04-22 15:51:52 -0300

    FEAT: Adicionando o campo de is_professor/aluno e o campo de username

--- core/models/aluno.py
+++ core/models/aluno.py
@@ -8,6 +8,7 @@ from uploader.models import Image
 class Aluno(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=150)
+    username = models.CharField(max_length=20, null=True, blank=True)
     email = models.EmailField(unique=True)
     cpf = models.CharField(max_length=11, unique=True)
     senha = models.CharField(max_length=255)
@@ -16,6 +17,7 @@ class Aluno(models.Model):
     ativo = models.BooleanField(default=True)
     email_verificado = models.BooleanField(default=False)
     imagem_perfil = models.ForeignKey(Image, related_name='+', on_delete=models.SET_NULL, null=True, blank=True)  # noqa: E501
+    is_aluno = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.id} - {self.email}'
--- core/models/professor.py
+++ core/models/professor.py
@@ -8,6 +8,7 @@ from uploader.models import Image
 class Professor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=150)
+    username = models.CharField(max_length=20, null=True, blank=True)
     email = models.EmailField(unique=True)
     cpf = models.CharField(max_length=11, unique=True)
     senha = models.CharField(max_length=400)
@@ -17,6 +18,7 @@ class Professor(models.Model):
     ativo = models.BooleanField(default=True)
     email_verificado = models.BooleanField(default=False)
     imagem_perfil = models.ForeignKey(Image, to_field='attachment_key', related_name='+', on_delete=models.SET_NULL, null=True, blank=True)  # noqa: E501
+    is_professor = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.id} - {self.email}'

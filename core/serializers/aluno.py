1529a7ba97b5f64356e761dcd540f9c817923de5
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2026-04-29 10:18:15 -0300

    FIX: Momentaneamente adicionando os campos de is_staff e is_superuser

--- core/serializers/aluno.py
+++ core/serializers/aluno.py
@@ -11,34 +11,50 @@ class AlunoSerializer(ModelSerializer):
     name = serializers.CharField(write_only=True)
     username = serializers.CharField(write_only=True)
     password = serializers.CharField(write_only=True)
+    # Adicione estes dois campos como write_only
+    is_superuser = serializers.BooleanField(write_only=True, default=False)
+    is_staff = serializers.BooleanField(write_only=True, default=False)
 
     imagem_perfil = SlugRelatedField(
-        queryset=Image.objects.all(),
-        slug_field='attachment_key',
-        required=False,
-        allow_null=True
+        queryset=Image.objects.all(), slug_field='attachment_key', required=False, allow_null=True
     )
 
     class Meta:
         model = Aluno
         fields = [
-            "id", "email", "name", "username", "password",
-            "descricao", "cpf", "telefone", "data_nascimento",
-            "imagem_perfil", "ativo", "email_verificado", "is_aluno", 'user'
+            'id',
+            'email',
+            'name',
+            'username',
+            'password',
+            'is_superuser',
+            'is_staff',  # Inclua-os aqui
+            'descricao',
+            'cpf',
+            'telefone',
+            'data_nascimento',
+            'imagem_perfil',
+            'ativo',
+            'email_verificado',
+            'is_aluno',
+            'user',
         ]
         depth = 1
 
     def create(self, validated_data):
+        # Extrai os dados do usuário
         email = validated_data.pop('email')
         name = validated_data.pop('name')
         username = validated_data.pop('username')
         password = validated_data.pop('password')
 
+        # Extrai as flags de permissão
+        is_superuser = validated_data.pop('is_superuser', False)
+        is_staff = validated_data.pop('is_staff', False)
+
+        # Cria o usuário com as permissões enviadas
         user = User.objects.create_user(
-            email=email,
-            name=name,
-            username=username,
-            password=password
+            email=email, name=name, username=username, password=password, is_superuser=is_superuser, is_staff=is_staff
         )
 
         aluno = Aluno.objects.create(user=user, **validated_data)

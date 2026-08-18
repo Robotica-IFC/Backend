fd98362dd620be00a249d15ff197559f23d99583
LucasFontini1 <lucasfontinidaluz@gmail.com>
2026-08-18 09:17:25 -0300

    FEAT: Tirando o unique de cpf do aluno e arrumando o serializer de projeto #28

--- core/serializers/projeto.py
+++ core/serializers/projeto.py
@@ -50,6 +50,7 @@ class ProjetoRetrieveSerializer(ModelSerializer):
 class ProjetoDetailWithPostsSerializer(ModelSerializer):
     image_perfil = ImageSerializer(read_only=True)
     equipe = CharField(source="equipe.nome", read_only=True)
+    equipe_id = CharField(source="equipe.id", read_only=True)
     instituicao = CharField(source="equipe.instituicao.nome", read_only=True)
     posts = PostListRetrieveSerializer(many=True, read_only=True)
 
